c49f5f2cd5abd74d1ca65877a3a4dd265f86d2fc
aahoolee <anviento@gmail.com>
2018-10-05 23:43:08 +0300

    flip auth

--- src/assets/scripts/auth.js
+++ src/assets/scripts/auth.js
@@ -1,1 +1,21 @@
 console.log("auth module loaded");
+
+const authBtn = document.querySelector(".js-auth-btn");
+const mainBtn = document.querySelector(".js-main-btn");
+const cardFront = document.querySelector(".welcome__card-front");
+const cardBack = document.querySelector(".welcome__card-back");
+
+
+authBtn.onclick = function () {
+    cardFront.style.transform = "rotateY(180deg)";
+    cardBack.style.transform = "rotateY(360deg)";
+    authBtn.style.display = 'none'
+
+};
+
+mainBtn.onclick = function () {
+    cardBack.style.transform = "rotateY(180deg)";
+    cardFront.style.transform = "rotateY(360deg)";
+    authBtn.style.display = 'block'
+};
+
